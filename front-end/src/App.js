5ccc9e637625a64d575da0bb31ff8fb16fc6ee2d
Aliasgar Merchant <amercha1@stevens.edu>
2024-06-22 13:25:00 +0200

    Add a simple react front-end

---
+++ front-end/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  const connectWallet = () => {
+    alert('Connect to Wallet clicked! (Wallet connection logic will go here)');
+  };
+
+  return (
+    <div className="container text-center">
+      <div className="row justify-content-center">
+        <div className="col-md-8">
+          <div className="welcome-screen">
+            <h1>Welcome to Crypto QuikPay</h1> 
+            <br></br>
+            <p><b>Crypto QuikPay</b> is a design-first innovative way to unify payments through an address such that any user can pay through any wallet and any currency while another user can receive a different currency in a different wallet.
+            All of this is abstracted from users, and done in a seamless yet elegant way.</p>
+            <button onClick={connectWallet} className="btn btn-primary">
+              Connect to Wallet
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
